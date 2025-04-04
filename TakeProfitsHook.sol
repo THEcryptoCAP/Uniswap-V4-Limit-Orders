788625a544c73bf1b6ac6b243d10874f1fe666d9
Avinash <cryptocapinoid@gmail.com>
2025-04-04 19:19:30 +0530

    Update TakeProfitsHook.sol

--- TakeProfitsHook.sol
+++ TakeProfitsHook.sol
@@ -9,15 +9,32 @@ import {BaseHook} from "v4-periphery/BaseHook.sol";
 import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
 import {Hooks} from "v4-core/libraries/Hooks.sol";
+import {PoolId, PoolIdLibrary} from "v4-core/types/PoolId.sol";
+import {PoolKey} from "v4-core/types/PoolKey.sol";
 
 contract TakeProfitsHook is BaseHook, ERC1155 {
+    // Use the PoolIdLibrary for PoolKey to add the `.toId()` function on a PoolKey
+    // which hashes the PoolKey struct into a bytes32 value
+    using PoolIdLibrary for PoolKey;
+
+    // Create a mapping to store the last known tickLower value for a given Pool
+    mapping(PoolId poolId => int24 tickLower) public tickLowerLasts;
+
+    // Create a nested mapping to store the take-profit orders placed by users
+    // The mapping is PoolId => tickLower => zeroForOne => amount
+    // PoolId => (...) specifies the ID of the pool the order is for
+    // tickLower => (...) specifies the tickLower value of the order i.e. sell when price is greater than or equal to this tick
+    // zeroForOne => (...) specifies whether the order is swapping Token 0 for Token 1 (true), or vice versa (false)
+    // amount specifies the amount of the token being sold
+    mapping(PoolId poolId => mapping(int24 tick => mapping(bool zeroForOne => int256 amount))) public takeProfitPositions;
+
     // initializing Basehook and ERC1155 parent contracts in the constructor 
-    constructor(
+     constructor(
         IPoolManager _poolManager,
         string memory _uri
     ) BaseHook(_poolManager) ERC1155(_uri) {}
 
-   
+
     // the logical workflow is as follows 
     // 1. A user creates a take-profit order through a placeOrder(…) function in our hook
     // 2. Our Hook, which is also an ERC-1155 contract, will mint ERC-1155 tokens to the user's wallet representing their order. 
@@ -49,7 +66,17 @@ contract TakeProfitsHook is BaseHook, ERC1155 {
                 afterDonate: false
         });
      }
+    
+    // Utility Helpers
+    function _setTickLowerLast(PoolId poolId, int24 tickLower) private {
+    tickLowerLasts[poolId] = tickLower;
+    }
 
-     
+    function _getTickLower(int24 actualTick, int24 tickSpacing) private pure returns (int24) {
+    int24 intervals = actualTick / tickSpacing;
+    if (actualTick < 0 && actualTick % tickSpacing != 0) intervals--; // round towards negative infinity
+    return intervals * tickSpacing;
+    } // explain this functionnnn 
 }
 
+   
